ae0570d882b37de7b93987d19103c141d7201d9f
ifgiovanni <contactargiovanni@gmail.com>
2023-11-24 15:48:53 -0600

    :sparkles: - added required log-viewer

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # logv-middleware-auth
  
+Add provider to config/app.php:
+
+Ifgiovanni\LogVMiddlewareAuth\Providers\LogVAuthRouteProvider::class
--- src/Middlewares/LogVAuthRouteMiddleware.php
+++ src/Middlewares/LogVAuthRouteMiddleware.php
@@ -17,7 +17,7 @@ class LogVAuthRouteMiddleware
      */
     public function handle($request, $next)
     {
-        if(app()->environment('local')){
+        if(app()->environment('production')){
             $token = request()->cookie(config('logv-middleware-auth.LOG_VIEWER_COOKIE'));
             if(!isset($token) || !$this->is_jwt_valid($token)){
                 return response()->json(['status' => 401, 'message' => "Unauthorized"]);
